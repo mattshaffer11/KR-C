224b9a79eadd67890635caaa7789fb38f80c3d78
Matt Shaffer <mattshaffer11@gmail.com>
2016-04-17 20:59:58 -0500

    Finish function

--- chapter3/itoa.c
+++ chapter3/itoa.c
@@ -1,20 +1,39 @@
 #include <stdio.h>
+#include <string.h>
 #include <math.h>
 
+void reverse(char str[], char newstr[]) {
+  int i;
+  int j = 0;
+
+  for (i = strlen(str) - 1; i >= 0; i--) {
+    newstr[j++] = str[i];
+  }
+
+  newstr[j] = '\0';
+}
+
+char itoc(int n) {
+  return n + '0';
+}
+
 void itoa(int num, char str[]) {
+  char tempstr[100];
   int result, modulo;
-  int digit = 1;
+  int digit = 0;
 
   do {
-    modulo = pow(10, digit);
+    modulo = pow(10, digit + 1);
     result = num % modulo;
     num -= result;
 
-    str[digit - 1] = (result / (modulo / 10)) + '0';
+    tempstr[digit] = itoc(result / (modulo / 10));
     digit++;
   } while (num >= modulo);
 
-  str[++digit] = '\0';
+  str[digit] = '\0';
+
+  reverse(tempstr, str);
 }
 
 int main() {
