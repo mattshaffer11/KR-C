2d2c342e98047d3b15f9796198b392bbfbd79af4
Matt Shaffer <mattshaffer11@gmail.com>
2016-04-18 20:31:14 -0500

    Create recursive string reverse

---
+++ chapter4/recursive_reverse.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+
+void swap(char str[], int i, int j) {
+  int temp = str[i];
+  str[i] = str[j];
+  str[j] = temp;
+}
+
+void recursive_reverse(char str[], int begin, int end) {
+  if (begin >= end) {
+    return;
+  }
+  else {
+    swap(str, begin, end);
+    recursive_reverse(str, ++begin, --end);
+  }
+}
+
+void reverse(char str[]) {
+  recursive_reverse(str, 0, strlen(str) - 1);
+}
+
+int main() {
+  char s[] = "hello";
+  reverse(s);
+  printf("%s\n", s);
+
+  return 0;
+}
