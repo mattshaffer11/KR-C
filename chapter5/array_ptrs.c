8339d56e35051f04a3c309e95c05318d0c4a6783
Matt Shaffer <mattshaffer11@gmail.com>
2016-05-25 07:09:28 -0500

    Refactor

--- chapter5/array_ptrs.c
+++ chapter5/array_ptrs.c
@@ -3,14 +3,14 @@
 void doubleArr(int *pa, int len) {
   int i;
   for (i = 0; i < len; i++) {
-    *(pa + i) = *(pa + i) * 2;
+    *(pa + i) *= 2;
   }
 }
 
 int main() {
   int nums[] = { 1, 2, 3 };
 
-  doubleArr(&nums[0], 3);
+  doubleArr(nums, 3);
 
   int i;
   for (i = 0; i < 3; i++) {
